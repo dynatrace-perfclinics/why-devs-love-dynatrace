6aa4b204030d45c7f103b7ea1ec56394783850a2
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2021-06-17 16:08:20 +0200

    entities ID for E3
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- monaco/devlove/set_dt_variables.sh
+++ monaco/devlove/set_dt_variables.sh
@@ -18,6 +18,10 @@ MIP_EASYTRAVEL=
 # Magic IP for KIAB (Keptn in a Box). Example if public ip == 1.2.3.4 then Magic IP => 1-2-3-4.nip.io 
 MIP_KIAB=
 
+# Call the function getEntityIds for setting the IDs. You'll need JQ, a Bash define the TenantURL and API_TOKEN 
+SERVICE_ID_CLASSIC=
+PG_ID_BACKEND=
+
 echo "usage 'source set_dt_variables.sh'"
 
 # FUNCTIONS DECLARATIONS
@@ -48,13 +52,17 @@ dynatracePrintValidateCredentials() {
         printInfo "Dynatrace User: $DT_USER"
         printInfo "Dynatrace EasyTravel MagicIP: $MIP_EASYTRAVEL"
         printInfo "Dynatrace KIAB MagicIP: $MIP_KIAB"
-    else
+        printInfo "ServiceId of ReverseProxy Classic: $SERVICE_ID_CLASSIC"
+        printInfo "PGID of ET BackEnd: $PG_ID_BACKEND"
+ else
         printError "Dynatrace Variables not set, check your variables"
         printError "Dynatrace Tenant: $DT_TENANT_URL"
         printError "Dynatrace API Token: $DT_API_TOKEN"
         printError "Dynatrace User: $DT_USER"
         printError "Dynatrace EasyTravel MagicIP: $MIP_EASYTRAVEL"
         printError "Dynatrace KIAB MagicIP: $MIP_KIAB"
+        printError "ServiceId of ReverseProxy Classic: $SERVICE_ID_CLASSIC"
+        printError "PGID of ET BackEnd: $PG_ID_BACKEND"
     fi
 }
 
@@ -65,6 +73,8 @@ exportVariables() {
     export DT_USER
     export MIP_EASYTRAVEL
     export MIP_KIAB
+    export SERVICE_ID_CLASSIC
+    export PG_ID_BACKEND
 }
 
 setMonacoNewCli(){
@@ -72,6 +82,20 @@ setMonacoNewCli(){
     export NEW_CLI=1
 }
 
+getEntityIds(){
+    printInfoSection "Getting the SERVICE-ID of the ReverseProxy of the Classic Application via cURL API"
+    SERVICE_ID_CLASSIC=$(curl -s GET "$DT_TENANT_URL/api/v2/entities?entitySelector=type(%22SERVICE%22),tag(%22classic-eval%22)&Api-Token=$DT_API_TOKEN" | jq -r '.entities[0].entityId')
+    printInfo "SERVICE_ID:$SERVICE_ID_CLASSIC"
+
+    printInfoSection "Getting the PROCESS-GROUP-ID of the Backend of the EasyTravel Application via cURL API"
+    PG_ID_BACKEND=$(curl -s GET "$DT_TENANT_URL/api/v2/entities?entitySelector=type(%22PROCESS_GROUP%22),tag(%22Stage:Staging%22),tag(%22EasyTravel:BackEnd%22),entityName(%22backend%22)&Api-Token=$DT_API_TOKEN" | jq -r '.entities[0].entityId')
+    printInfo "PG_ID_BACKEND:$PG_ID_BACKEND"
+}
+
+getEntityIds
+
 dynatracePrintValidateCredentials
+
 exportVariables
+
 setMonacoNewCli
